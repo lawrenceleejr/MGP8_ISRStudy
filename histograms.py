187203f52a0444707fa325cdfedee0f762e49a44
Colby Thompson <colbythomps@gmail.com>
2023-10-19 15:24:26 -0400

    Updated histograms.py and created folder for hists along with 2 hists

--- histograms.py
+++ histograms.py
@@ -2,49 +2,57 @@ from matplotlib import pyplot as plt
 import numpy as np
 import uproot
 
-def transform_array_to_midpoint_array(arr):
-    l = arr.tolist()
-    new_arr = []
-    for i in range(len(l)):
-        try:
-            new_arr.insert(i, (l[i] + l[i+1])/2)
-        except IndexError:
-            break
 
-    return new_arr
+def prepare_array_for_hist(frequency, bins):
+    '''
+    :param frequency: The array that contains the frequency data (y axis values on hist)
+    :param bins: The array that contains the bins (x axis on hist)
+    :return: Converts the frequency array to a form better suited for histograms
+    '''
+    arr = np.array([])
+    for i in range(len(frequency)):
+        arr = np.concatenate((arr, np.repeat(bins[i], frequency[i])))
 
-def gluglu_pT_range_plot(rootfilepath, mass, savefig=None):
-    with uproot.open(rootfilepath) as root:
-        maxes = np.zeros(56)
-        mins = np.full(56, 100000000)
-        bins = []
-        for key in root.keys():
-            if 'pTsum' in key:
-                arr = root[key].to_numpy()
-                for i in range(len(arr[0])):
-                    if arr[0][i] > maxes[i]:
-                        maxes[i] = arr[0][i]
-                    if arr[0][i] < mins[i]:
-                        mins[i] = arr[0][i]
-                bins = transform_array_to_midpoint_array(arr[1])
-        mids = []
-        for i in range(len(maxes)):
-            mids.append(maxes[i]-mins[i])
+    return arr
 
-        plt.plot(bins, mids)
-        plt.fill_between(bins, mins, maxes, alpha=0.5, color='dodgerblue')
-        plt.suptitle("Min/Max {} GeV".format(mass))
-        plt.xlabel("gluglu pT (GeV)")
-        plt.ylabel("Weighted Frequency")
+def weighted_sum_histograms(masses, rootpaths, log=False, savefig=None):
+    '''
+    :param mass: List of root file masses
+    :param rootpath: List of paths to the root files
+    :param log: If True, transforms the yscale of the histogram to log scale
+    :param savefig: Filepath to save the histogram, does not save by default
+    :return: Histogram of the weighted gluglu pT sums
+    '''
+    for i in range(len(masses)):
+        with uproot.open(rootpaths[i]) as root:
+            final_arr = np.zeros(56)
+            for key in root.keys():
+                if 'pTsum' in key:
+                    arr = root[key].to_numpy()
+                    final_arr += arr[0]
 
-        if savefig!=None:
-            plt.savefig(savefig)
-        else:
-            plt.show()
+            bins = arr[1]
+            frequency = prepare_array_for_hist(final_arr, bins)
+            plt.hist(frequency, bins, histtype=u'step', label="Glu Mass {} GeV".format(masses[i]))
 
-masses = [1000, 1400, 1600, 1800, 2000, 2200, 2400, 2600]
-for mass in masses:
-    rootpath = "/Users/colbythompson/Documents/Research/gluglu_pT/gluglu_MGn50_GeV{}.root".format(mass)
-    with uproot.open(rootpath) as root:
-        print("{}\n{}".format(mass, root.keys()))
-    #gluglu_pT_range_plot(rootpath, 1000)
+    plt.suptitle("Sum of Weighted Histograms for Various Gluino Masses")
+    plt.xlabel("gluglu pT (GeV)")
+    plt.legend()
+    if log:
+        plt.ylabel("Frequency (log)")
+        plt.yscale('log')
+    else:
+        plt.ylabel("Frequency")
+    if savefig!=None:
+        plt.savefig(savefig)
+    else:
+        plt.show()
+
+masses = [1400, 1600, 1800, 2000, 2200, 2400]
+rootpaths = ["/Users/colbythompson/PycharmProjects/MGP8_ISRStudy_LAdev/output-files/gluglu_MGn50_GeV1400.root",
+             "/Users/colbythompson/PycharmProjects/MGP8_ISRStudy_LAdev/output-files/gluglu_MGn50_GeV1600.root",
+             "/Users/colbythompson/PycharmProjects/MGP8_ISRStudy_LAdev/output-files/gluglu_MGn50_GeV1800.root",
+             "/Users/colbythompson/PycharmProjects/MGP8_ISRStudy_LAdev/output-files/gluglu_MGn50_GeV2000.root",
+             "/Users/colbythompson/PycharmProjects/MGP8_ISRStudy_LAdev/output-files/gluglu_MGn50_GeV2200.root",
+             "/Users/colbythompson/PycharmProjects/MGP8_ISRStudy_LAdev/output-files/gluglu_MGn50_GeV2400.root"]
+weighted_sum_histograms(masses, rootpaths, True, savefig="/Users/colbythompson/PycharmProjects/MGP8_ISRStudy_LAdev/histograms/weightedHistSumLog.png")
